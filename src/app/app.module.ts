b26f7cae5eb672a1645f65cd5ac370a75df14443
Informática DP <61481001+infodp@users.noreply.github.com>
2022-03-30 17:22:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ProvidersComponent } from './components/providers/providers.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CustomersComponent,
+    ProvidersComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component:HomeComponent},
+      {path: 'customers', component:CustomersComponent},
+      {path: 'providers', component:ProvidersComponent},
+      {path: '**', component:PageNotFoundComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
